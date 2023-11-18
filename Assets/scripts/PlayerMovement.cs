bb1a52b439056714beadc73e90de6e49ddb3f53b
elevltu <elev@ltu.edu>
2023-11-18 18:35:46 -0500

    probably fixed now

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -15,7 +15,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        /*if (Input.GetKeyDown(KeyCode.W))
         {
             rb.AddForce(new Vector2(speed * Time.deltaTime, 0), ForceMode2D.Impulse);
         }
@@ -30,6 +30,22 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             rb.AddForce(new Vector2(0, -speed * Time.deltaTime), ForceMode2D.Impulse);
+        }*/
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(rb.velocity.y, speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector2(rb.velocity.y, -speed);
         }
     }
 }
