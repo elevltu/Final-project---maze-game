b9389e46b0748f7baae886b7ad9aaf30293296a8
elevltu <elev@ltu.edu>
2023-11-19 13:21:22 -0500

    box can no longer get stuck against the wall

---
+++ Assets/scripts/MoveAwayFromWall.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAwayFromWall : MonoBehaviour
+{
+    public int pushForce = 1;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(UnityEngine.Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("WallLeft"))
+        {
+            rb.position = new Vector2(rb.position.x+pushForce, rb.position.y);
+        }
+        if (collision.gameObject.CompareTag("WallRight"))
+        {
+            rb.position = new Vector2(rb.position.x-pushForce, rb.position.y);
+        }
+        if (collision.gameObject.CompareTag("WallTop"))
+        {
+            rb.position = new Vector2(rb.position.x, rb.position.y-(pushForce*2));
+        }
+        if (collision.gameObject.CompareTag("WallBottom"))
+        {
+            rb.position = new Vector2(rb.position.x, rb.position.y+(pushForce*2));
+        }
+    }
+}
