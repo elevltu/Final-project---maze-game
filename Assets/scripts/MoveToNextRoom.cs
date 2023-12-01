7beb8467025cd8705a8247f055f4f710eb233970
elevltu <elev@ltu.edu>
2023-12-01 14:31:38 -0500

    background moves

--- Assets/scripts/MoveToNextRoom.cs
+++ Assets/scripts/MoveToNextRoom.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Direction
+{
+    left,
+    down,
+    right,
+    up
+}
 public class MoveToNextRoom : MonoBehaviour
 {
     public Camera thisRoomCamera;
     public Camera previousRoomCamera;
     private AudioListener thisRoomAudioListener;
     private AudioListener previousRoomAudioListener;
+    public SpriteRenderer background;
+    public Direction direction;
     // Start is called before the first frame update
     private void Start()
     {
@@ -22,6 +31,18 @@ public class MoveToNextRoom : MonoBehaviour
             previousRoomCamera.enabled = false;
             thisRoomAudioListener.enabled = true;
             previousRoomAudioListener.enabled = false;
+            if (direction == Direction.up) {
+                background.transform.position = new Vector3(background.transform.position.x, background.transform.position.y + (float)(8.9783163 / 3), 2);
+            }
+            if (direction == Direction.down) {
+                background.transform.position = new Vector3(background.transform.position.x, background.transform.position.y - (float)(8.9783163 / 3), 2);
+            }
+            if (direction == Direction.left) {
+                background.transform.position = new Vector3(background.transform.position.x - (float)(8.9783163 / 3), background.transform.position.y, 2);
+            }
+            if (direction == Direction.right){
+                background.transform.position = new Vector3(background.transform.position.x + (float)(8.9783163 / 3), background.transform.position.y, 2);
+            }
         }
     }
 }
