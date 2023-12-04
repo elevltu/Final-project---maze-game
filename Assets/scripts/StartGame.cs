c1b6c1e7b1ae44a77bcabfe881eff6a1e660fbad
Deerfish2 <118629478+Deerfish2@users.noreply.github.com>
2023-12-04 11:13:54 -0500

    main menu

---
+++ Assets/scripts/StartGame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public string sceneName;
+    // Start is called before the first frame update
+    public void loadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
